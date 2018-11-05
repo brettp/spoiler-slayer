757e3bf71c2a2ad465264805a88972ae00616224
Brett Profitt <brett.profitt@gmail.com>
2018-11-05 16:59:35 -0500

    Using native classLists

--- src/options/lib/util.js
+++ src/options/lib/util.js
@@ -1,46 +1,12 @@
-export function getClasses(el) {
-    return el.className.split(' ').filter(v => v.trim() !== '');
-}
-
-export function setClasses(el, classes) {
-    // use a set to make it unique
-    // ...except babel doesn't support it.
-    // let newClasses = [...new Set(classes)];
-    let newClasses = [];
-    for (let cls of classes) {
-        if (!newClasses.includes(cls)) {
-            newClasses.push(cls);
-        }
-    }
-
-    el.className = newClasses.join(' ');
-}
-
-export function addClass(el, cls) {
-    let classes = getClasses(el);
-    classes.push(cls);
-    setClasses(el, classes);
-}
-
-export function removeClass(el, cls) {
-    let classes = getClasses(el);
-    let i = classes.indexOf(cls);
-
-    if (i > 0) {
-        classes.splice(i, 1);
-        setClasses(el, classes);
-    }
-}
-
 export function addFlash(el, type = 'fail') {
     el.addEventListener(
         'animationend',
         e => {
-            removeClass(el, 'save-fail');
-            removeClass(el, 'save-success');
+            el.classList.remove('save-fail');
+            el.classList.remove('save-success');
         },
         {once: true}
     );
 
-    addClass(el, `save-${type}`);
+    el.classList.add(`save-${type}`);
 }
